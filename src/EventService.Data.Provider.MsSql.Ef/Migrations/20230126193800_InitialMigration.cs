11f40a530bbd8e5010dc9d875fe5eed6a112be81
Liza Negotiv <ln459@hotmail.com>
2023-01-30 15:39:43 +0300

    Changes taking into account DB models

---
+++ src/EventService.Data.Provider.MsSql.Ef/Migrations/20230126193800_InitialMigration.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using EventService.Data.Provider.MsSql.Ef;
+using LT.DigitalOffice.EventService.Models.Db;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LT.DigitalOffice.EventService.Data.Provider.MsSql.Ef.Migrations;
+
+[DbContext(typeof(EventServiceDbContext))]
+[Migration("20230126193800_InitialMigration")]
+public class _20230126193800_InitialMigration : Migration
+{
+  private void CreateEventTable(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.CreateTable(
+    DbEvent.TableName,
+    columns: table => new
+    {
+      Id = table.Column<Guid>(nullable: false),
+      Name = table.Column<string>(nullable: false),
+      Address = table.Column<string>(nullable: false),
+      Description = table.Column<string>(nullable: true),
+      Date = table.Column<DateTime>(nullable: false),
+      Format = table.Column<int>(nullable: false),
+      Access = table.Column<int>(nullable: false),
+      IsActive = table.Column<bool>(nullable: false),
+      CreatedBy = table.Column<Guid>(nullable: false),
+      CreatedAtUtc = table.Column<DateTime>(nullable: false),
+      ModifiedBy = table.Column<Guid>(nullable: true),
+      ModifiedAtUtc = table.Column<DateTime>(nullable: true)
+    },
+
+    constraints: table =>
+    {
+      table.PrimaryKey($"PK_{DbEvent.TableName}", e => e.Id);
+    });
+  }
+
+  private void CreateEventCategoryTable(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.CreateTable(
+    DbEventCategory.TableName,
+    columns: table => new
+    {
+      Id = table.Column<Guid>(nullable: false),
+      EventId = table.Column<Guid>(nullable: false),
+      CategoryId = table.Column<Guid>(nullable: false),
+      CreatedBy = table.Column<Guid>(nullable: false),
+      CreatedAtUtc = table.Column<DateTime>(nullable: false)
+    },
+
+    constraints: table =>
+    {
+      table.PrimaryKey($"PK_{DbEventCategory.TableName}", e => e.Id);
+    });
+  }
+
+  private void CreateCategoryTable(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.CreateTable(
+    DbCategory.TableName,
+    columns: table => new
+    {
+      Id = table.Column<Guid>(nullable: false),
+      Name = table.Column<string>(nullable: false),
+      Color = table.Column<int>(nullable: false),
+      IsActive = table.Column<bool>(nullable: false),
+      CreatedBy = table.Column<Guid>(nullable: false),
+      CreatedAtUtc = table.Column<DateTime>(nullable: false),
+      ModifiedBy = table.Column<Guid>(nullable: true),
+      ModifiedAtUtc = table.Column<DateTime>(nullable: true)
+    },
+
+    constraints: table =>
+    {
+      table.PrimaryKey($"PK_{DbCategory.TableName}", e => e.Id);
+    });
+  }
+
+  private void CreateEventUserTable(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.CreateTable(
+    DbEventUser.TableName,
+    columns: table => new
+    {
+      Id = table.Column<Guid>(nullable: false),
+      EventId = table.Column<Guid>(nullable: false),
+      UserId = table.Column<Guid>(nullable: false),
+      Status = table.Column<int>(nullable: false),
+      NotifyAtUtc = table.Column<DateTime>(nullable: true),
+      CreatedBy = table.Column<Guid>(nullable: false),
+      CreatedAtUtc = table.Column<DateTime>(nullable: false),
+      ModifiedBy = table.Column<Guid>(nullable: true),
+      ModifiedAtUtc = table.Column<DateTime>(nullable: true)
+    },
+
+    constraints: table =>
+    {
+      table.PrimaryKey($"PK_{DbEventUser.TableName}", e => e.Id);
+    });
+  }
+
+  private void CreateEventCommentTable(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.CreateTable(
+    DbEventComment.TableName,
+    columns: table => new
+    {
+      Id = table.Column<Guid>(nullable: false),
+      Content = table.Column<string>(nullable: false),
+      UserId = table.Column<Guid>(nullable: false),
+      EventId = table.Column<Guid>(nullable: false),
+      ParentId = table.Column<Guid>(nullable: true),
+      IsActive = table.Column<bool>(nullable: false),
+      CreatedAtUtc = table.Column<DateTime>(nullable: false),
+      ModifiedBy = table.Column<Guid>(nullable: true),
+      ModifiedAtUtc = table.Column<DateTime>(nullable: true)        
+    },
+
+    constraints: table =>
+    {
+      table.PrimaryKey($"PK_{DbEventComment.TableName}", e => e.Id);
+    });
+  }
+
+  private void CreateEventImageTable(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.CreateTable(
+    DbEventImage.TableName,
+    columns: table => new
+    {
+      Id = table.Column<Guid>(nullable: false),
+      EventId = table.Column<Guid>(nullable: false),
+      ImageId = table.Column<Guid>(nullable: false),
+      CreatedBy = table.Column<Guid>(nullable: false),
+      CreatedAtUtc = table.Column<DateTime>(nullable: false),
+    },
+
+    constraints: table =>
+    {
+      table.PrimaryKey($"PK_{DbEventImage.TableName}", e => e.Id);
+    });
+  }
+
+  private void CreateEventFileTable(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.CreateTable(
+    DbEventFile.TableName,
+    columns: table => new
+    {
+      Id = table.Column<Guid>(nullable: false),
+      EventId = table.Column<Guid>(nullable: false),
+      FileId = table.Column<Guid>(nullable: false),
+      CreatedBy = table.Column<Guid>(nullable: false),
+      CreatedAtUtc = table.Column<DateTime>(nullable: false),
+    },
+
+    constraints: table =>
+    {
+      table.PrimaryKey($"PK_{DbEventFile.TableName}", e => e.Id);
+    });
+  }
+
+  protected override void Up(MigrationBuilder migrationBuilder)
+  {
+    CreateEventTable(migrationBuilder);
+    CreateEventCategoryTable(migrationBuilder);
+    CreateCategoryTable(migrationBuilder);
+    CreateEventUserTable(migrationBuilder);
+    CreateEventCommentTable(migrationBuilder);
+    CreateEventImageTable(migrationBuilder);
+    CreateEventFileTable(migrationBuilder);
+  }
+
+  protected override void Down(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.DropTable(DbEvent.TableName);
+    migrationBuilder.DropTable(DbEventCategory.TableName);
+    migrationBuilder.DropTable(DbCategory.TableName);
+    migrationBuilder.DropTable(DbEventUser.TableName);
+    migrationBuilder.DropTable(DbEventComment.TableName);
+    migrationBuilder.DropTable(DbEventImage.TableName);
+    migrationBuilder.DropTable(DbEventFile.TableName);
+  }
+}
