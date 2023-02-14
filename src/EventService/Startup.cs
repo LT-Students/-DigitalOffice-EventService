516b35136ea92bba9510e83754849019eef667d5
Liza Negotiv <ln459@hotmail.com>
2023-02-14 17:45:54 +0300

    add create

--- src/EventService/Startup.cs
+++ src/EventService/Startup.cs
@@ -18,9 +18,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Text.Json.Serialization;
 
 namespace EventService
 {
