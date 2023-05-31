217d5a179345e5a6057317cf01da0260ce4a77f4
Vladimir Liamin <LyaminVA@yandex.ru>
2023-05-31 14:28:47 +0300

    small fix

--- src/EventService.Models.Dto/Models/CommentInfo.cs
+++ src/EventService.Models.Dto/Models/CommentInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace LT.DigitalOffice.EventService.Models.Dto.Models
+namespace LT.DigitalOffice.EventService.Models.Dto.Models;
+
+public class CommentInfo
 {
-  public class CommentInfo
-  {
-    public Guid Id { get; set; }
-    public string Content { get; set; }
-    public Guid UserId { get; set; }
-    public Guid? ParentId { get; set; }
-    public List<CommentInfo> Comment { get; set; }
-  }
+  public Guid Id { get; set; }
+  public string Content { get; set; }
+  public Guid UserId { get; set; }
+  public Guid? ParentId { get; set; }
+  public List<CommentInfo> Comment { get; set; }
 }
