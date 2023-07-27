3b64a9aa8933e072cf33b90d5209b7a4a8b2931e
DIO <meserious1@gmail.com>
2023-07-27 20:07:57 +0300

    create test projects

---
+++ test/EventService.Business.UnitTests/Commands/Event/CreateEventCommandTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using LT.DigitalOffice.EventService.Business.Commands.Event.Interfaces;
+using LT.DigitalOffice.EventService.Models.Db;
+using LT.DigitalOffice.EventService.Models.Dto.Requests.Event;
+using LT.DigitalOffice.Kernel.Helpers.Interfaces;
+using LT.DigitalOffice.Kernel.Responses;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Moq.AutoMock;
+using NUnit.Framework;
+
+namespace LT.DigitalOffice.EventService.Business.UnitTests.Commands.Event;
+
+public class CreateEventCommandTests
+{
+  private AutoMocker _autoMocker;
+  private ICreateEventCommand _command;
+
+  private CreateEventRequest _request;
+  private DbEvent _dbEvent;
+
+  [OneTimeSetUp]
+  public void OneTimeSetUp()
+  {
+    
+
+  }
+
+  [SetUp]
+  public void Setup()
+  {
+
+  }
+}
---
+++ test/EventService.Business.UnitTests/Commands/Event/CreateEventCommandTestsOtherVersion.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LT.DigitalOffice.EventService.Business.UnitTests.Commands.Event;
+
+public class CreateEventCommandTestsOtherVersion
+{
+  [OneTimeSetUp]
+  public void OneTimeSetUp()
+  {
+
+  }
+
+  [SetUp]
+  public void Setup()
+  {
+
+  }
+}
